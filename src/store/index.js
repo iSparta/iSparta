d2285ec692d14facf82a8a2f568cb3bf203afcb2
jeakey <june_fh@qq.com>
2019-08-30 20:41:28 +0800

    【bug fix】修复初次使用时读取缓存错误的问题

--- src/store/index.js
+++ src/store/index.js
@@ -8,16 +8,28 @@ const fs = require("fs-extra");
 const storage = require('electron-localstorage');
 const os = require("os");
 const path = require("path");
-if(process.env.NODE_ENV=="development"){
-  storage.setStoragePath(path.join(os.tmpdir(), 'iSparta/localstorage-dev.json'));
-}else{
-  storage.setStoragePath(path.join(os.tmpdir(), 'iSparta/localstorage.json'));
+// 【bug fix】修复初次使用时读取缓存错误的问题
+let storagePath = "";
+if (process.env.NODE_ENV == "development") {
+
+  storagePath = path.join(os.tmpdir(), 'iSparta/localstorage-dev.json');
+} else {
+  storagePath = path.join(os.tmpdir(), 'iSparta/localstorage.json');
 }
+if (!fs.existsSync(storagePath)) {
+  fs.ensureFileSync(storagePath)
+
+}
+if (fs.readFileSync(storagePath)) {
+  fs.writeFileSync(storagePath, "{}", "utf-8")
+}
+
+storage.setStoragePath(storagePath);
 window.storage = storage;
 Vue.use(Vuex)
 // 原始数据
 const defaultState = {
-  language:'zh-cn',
+  language: 'zh-cn',
   options: {
     'frameRate': 20,
     'loop': 0,
@@ -59,9 +71,9 @@ var state = {
 //init globalSetting
 var globalSetting = window.storage.getItem('globalSetting');
 
-if(!globalSetting){
-  
-  let tempSetting  = defaultState;
+if (!globalSetting) {
+
+  let tempSetting = defaultState;
   window.storage.setItem('globalSetting', JSON.stringify(tempSetting))
 }
 // get localstorage data
@@ -69,17 +81,17 @@ var localData = window.storage.getItem('iSparta-item')
 if (localData) {
   // 取loaclstorage时重置进度
   var localItems = JSON.parse(localData)
-  let items=[];
+  let items = [];
   _.each(localItems, function (item) {
-    let isError=false;
-    for(let i=0;i<item.basic.fileList.length;i++){
-      
-      if (!fs.existsSync(item.basic.fileList[i])){
-        isError=true;
+    let isError = false;
+    for (let i = 0; i < item.basic.fileList.length; i++) {
+
+      if (!fs.existsSync(item.basic.fileList[i])) {
+        isError = true;
         break;
       }
     }
-    if(!isError){
+    if (!isError) {
       item.process.text = ''
       item.process.schedule = 0;
       items.push(item);
@@ -89,7 +101,7 @@ if (localData) {
 }
 // 只有这里才能才state的值
 const mutations = {
-  [types.ITEMS_ADD] (state, data) {
+  [types.ITEMS_ADD](state, data) {
     if (state.locked) {
       return false
     }
@@ -102,7 +114,7 @@ const mutations = {
     state.items.push(itemData)
     storage.setItem('iSparta-item', JSON.stringify(state.items))
   },
-  [types.ITEMS_REMOVE] (state) {
+  [types.ITEMS_REMOVE](state) {
     if (state.locked) {
       return false
     }
@@ -119,11 +131,11 @@ const mutations = {
     if (state.locked) {
       return false
     }
-    
+
     state.items = []
     storage.setItem('iSparta-item', JSON.stringify(state.items))
   },
-  [types.ITEMS_EDIT_BASIC] (state, keyValue) {
+  [types.ITEMS_EDIT_BASIC](state, keyValue) {
     if (state.locked) {
       return false
     }
@@ -132,7 +144,7 @@ const mutations = {
     _.extend(selectedBasic, keyValue)
     storage.setItem('iSparta-item', JSON.stringify(state.items))
   },
-  [types.ITEMS_EDIT_OPTIONS] (state, keyValue) {
+  [types.ITEMS_EDIT_OPTIONS](state, keyValue) {
     if (state.locked) {
       return false
     }
@@ -140,10 +152,10 @@ const mutations = {
     var selectedOption = selectedItem[0].options
     _.extend(selectedOption, keyValue)
     storage.setItem('iSparta-item', JSON.stringify(state.items))
-                // var new = _.merge(selectedOption,keyValue)
-                // console.log(keyValue)
+    // var new = _.merge(selectedOption,keyValue)
+    // console.log(keyValue)
   },
-  [types.ITEMS_EDIT_MULTI_OPTIONS] (state, keyValue) {
+  [types.ITEMS_EDIT_MULTI_OPTIONS](state, keyValue) {
     if (state.locked) {
       return false
     }
@@ -152,16 +164,16 @@ const mutations = {
     })
     storage.setItem('iSparta-item', JSON.stringify(state.items))
   },
-  [types.ITEMS_EDIT_PROCESS] (state, keyValue) {
+  [types.ITEMS_EDIT_PROCESS](state, keyValue) {
     var selectedItem = _.filter(state.items, { isSelected: true })
     // console.warn(keyValue)
     var selectedProcess = selectedItem[keyValue.index].process
     _.extend(selectedProcess, keyValue)
 
-          // 进度不记录在localstore里
-          // storage.setItem("iSparta-item",JSON.stringify(state.items));
+    // 进度不记录在localstore里
+    // storage.setItem("iSparta-item",JSON.stringify(state.items));
   },
-  [types.SINGLE_SELECT] (state, index) {
+  [types.SINGLE_SELECT](state, index) {
     if (state.locked) {
       return false
     }
@@ -171,21 +183,21 @@ const mutations = {
     state.items[index].isSelected = true
     storage.setItem('iSparta-item', JSON.stringify(state.items))
   },
-  [types.SET_SELECTED] (state, index) {
+  [types.SET_SELECTED](state, index) {
     if (state.locked) {
       return false
     }
     state.items[index].isSelected = true
     storage.setItem('iSparta-item', JSON.stringify(state.items))
   },
-  [types.MULTI_SELECT] (state, index) {
+  [types.MULTI_SELECT](state, index) {
     if (state.locked) {
       return false
     }
     state.items[index].isSelected = !state.items[index].isSelected
     storage.setItem('iSparta-item', JSON.stringify(state.items))
   },
-  [types.ALL_SELECTED] (state) {
+  [types.ALL_SELECTED](state) {
     if (state.locked) {
       return false
     }
@@ -194,72 +206,72 @@ const mutations = {
     })
     storage.setItem('iSparta-item', JSON.stringify(state.items))
   },
-  [types.SET_LOCK] (state, boolean) {
+  [types.SET_LOCK](state, boolean) {
     state.locked = boolean
   }
 
 }
-    // actions are functions that cause side effects and can involve
-    // asynchronous operations.
-    // 主要处理异步事件
+// actions are functions that cause side effects and can involve
+// asynchronous operations.
+// 主要处理异步事件
 const actions = {
 
-    //
-  add (context, data) {
+  //
+  add(context, data) {
     context.commit('ITEMS_ADD', data)
   },
-  remove (context) {
+  remove(context) {
     context.commit('ITEMS_REMOVE')
   },
   removeAll(context) {
     context.commit('ALL_REMOVE')
   },
-  editBasic (context, keyValue) {
+  editBasic(context, keyValue) {
     context.commit('ITEMS_EDIT_BASIC', keyValue)
   },
-  editOptions (context, keyValue) {
+  editOptions(context, keyValue) {
     context.commit('ITEMS_EDIT_OPTIONS', keyValue)
   },
-  editMultiOptions (context, keyValue) {
+  editMultiOptions(context, keyValue) {
     context.commit('ITEMS_EDIT_MULTI_OPTIONS', keyValue)
   },
-  editProcess (context, keyValue) {
+  editProcess(context, keyValue) {
     context.commit('ITEMS_EDIT_PROCESS', keyValue)
   },
-  setSelected (context, index) {
+  setSelected(context, index) {
     context.commit('SET_SELECTED', index)
   },
-  singleSelect (context, index) {
+  singleSelect(context, index) {
     context.commit('SINGLE_SELECT', index)
   },
-  multiSelect (context, index) {
+  multiSelect(context, index) {
     context.commit('MULTI_SELECT', index)
   },
-  allSelect (context) {
+  allSelect(context) {
     context.commit('ALL_SELECTED')
   },
-  setLock (context, boolean) {
+  setLock(context, boolean) {
     context.commit('SET_LOCK', boolean)
   }
 }
 
 // 处理一些分发的事件
 const getters = {
-    // 获取items
-  getterItems () {
+  // 获取items
+  getterItems() {
     return state.items
   },
-    // 获取锁的状态
-  getterLocked () {
+  // 获取锁的状态
+  getterLocked() {
     return state.locked
   },
-    // 获取选中的items
-  getterSelected () {
-    return _.filter(state.items, {isSelected: true})
+  // 获取选中的items
+  getterSelected() {
+    return _.filter(state.items, { isSelected: true })
   },
-    // 获取选中items的index
-  getterSelectedIndex () {
-    return _.findIndex(state.items, {isSelected: true})
+  // 获取选中items的index
+  getterSelectedIndex() {
+    return _.findIndex(state.items, { isSelected: true })
   }
 }
 
